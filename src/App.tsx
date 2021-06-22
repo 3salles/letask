2bc3c009d33f988adeb37913e0f8e066b3df1174
Beatriz Salles <beatrizsallesss@gmail.com>
2021-06-22 17:52:11 -0300

    :sparkles: feat: Create navigation between home and create room pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import Home from "./pages/Home"
 import NewRoom from "./pages/NewRoom";
 import GlobalStyle from "./styles/global";
 
+import { BrowserRouter, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
     <GlobalStyle />
-    <NewRoom />
-    </>
+    <Route path="/" exact component={Home} />
+    <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
