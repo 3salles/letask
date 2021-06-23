b66c6a3790dada6b0617d81de08037713cfd172b
Beatriz Salles <beatrizsallesss@gmail.com>
2021-06-23 08:30:53 -0300

    :sparkles: feat: Create firebase database room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,8 +1,10 @@
-import logoImg from "../assets/images/logo.svg";
-
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+
+import logoImg from "../assets/images/logo.svg";
 
 import Aside from "../components/Aside";
 import Button from "../components/Button";
@@ -10,9 +12,26 @@ import Button from "../components/Button";
 import { Container, Main, Content, Form } from "../styles/pages/auth";
 
 
-
 const NewRoom = () => {
   const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState("");
+  const [disable, setDisable] = useState(false);
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === ''){
+      // setDisable(true);
+      return
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+  }
 
   return (
     <Container>
@@ -21,9 +40,14 @@ const NewRoom = () => {
         <Content>
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <Form>
-            <input type="text" placeholder="Nome da sala" />
-            <Button type="submit">Criar a sala</Button>
+          <Form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit" disabled={disable}>Criar a sala</Button>
           </Form>
           <p>
             Quer entrar em uma sala já existente?
