75699c463d143c61cd9f6fdc5327139ee3ab2323
Beatriz Salles <beatrizsallesss@gmail.com>
2021-06-23 11:09:17 -0300

    :sparkles: feat: Add send new question to firebase

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,33 +1,75 @@
+import { FormEvent, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Button from "../components/Button";
 import Header from "../components/Header";
 
-import { RoomParams } from "../components/Header"
+import { RoomParams } from "../components/Header";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
-import { Container, RoomTitle, Textarea, FormFooter } from '../styles/pages/room'
-
+import {
+  Container,
+  RoomTitle,
+  Textarea,
+  FormFooter,
+} from "../styles/pages/room";
 
 const Room = () => {
+  const [newQuestion, setNewQuestion] = useState("");
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
+  const roomId = params.id;
+  
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    
+    if (newQuestion.trim() === ''){
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in');
+      // TODO: Add toast
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlihted: false,
+      isAnswered: false,
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+  }
 
   return (
     <>
-      <Header id={params.id} />
+      <Header id={roomId} />
       <Container>
         <RoomTitle>
           <h1>Sala React</h1>
           <span>4 perguntas</span>
         </RoomTitle>
 
-        <form>
-          <Textarea placeholder="O que você quer perguntar?" />
+        <form onSubmit={handleSendQuestion}>
+          <Textarea
+            placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <FormFooter>
             <span>
-              Para enviar uma pergunta, 
+              Para enviar uma pergunta,
               <button>faça seu login</button>.
             </span>
-            <Button type="submit">Enviar pergunta</Button>
+            <Button type="submit" disabled={!user || !newQuestion}>
+              Enviar pergunta
+            </Button>
           </FormFooter>
         </form>
       </Container>
